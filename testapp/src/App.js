78d46f0afef9fe0bc58a81cda827e340f4d9085d
Danny Hwang <dannyhwangca@gmail.com>
2020-04-25 17:33:25 -0700

    added firebase to package

--- testapp/src/App.js
+++ testapp/src/App.js
@@ -1,6 +1,8 @@
 // Import React
 import React, { Component } from 'react';
 import {Router} from '@reach/router';
+import firebase from './Firebase';
+
 import Home from './Home';
 import Welcome from './Welcome';
 import Navigation from './Navigation'
---
+++ testapp/src/Firebase.js
@@ -0,0 +1,22 @@
+import firebase from 'firebase/app';
+import 'firebase/database';
+import 'firebase/auth';
+
+var firebaseConfig = {
+    apiKey: "AIzaSyBxZARUsRA3yxzwMjnaaZZNlVSIUxPeSUk",
+    authDomain: "react-spas-820df.firebaseapp.com",
+    databaseURL: "https://react-spas-820df.firebaseio.com",
+    projectId: "react-spas-820df",
+    storageBucket: "react-spas-820df.appspot.com",
+    messagingSenderId: "404797110143",
+    appId: "1:404797110143:web:594ea5e396af11d8c18c4a",
+    measurementId: "G-7ELGMRMZ9P"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
+
+export const provider = new firebase.auth.GoogleAuthProvider();
+export const auth = firebase.auth();
+
+export default firebase;
