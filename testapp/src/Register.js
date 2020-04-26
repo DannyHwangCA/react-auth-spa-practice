cc8d2236e89ae7bb4b8cbb725943fa400026247c
Danny Hwang <dannyhwangca@gmail.com>
2020-04-25 22:45:30 -0700

    pushed user state info from register to app.js and added firebase auth change logic

--- testapp/src/Register.js
+++ testapp/src/Register.js
@@ -3,8 +3,8 @@ import FormError from './FormError';
 import firebase from './Firebase';
 
 class Register extends Component {
-    constructor() {
-        super();
+    constructor(props) {
+        super(props);
         this.state = {
             displayName: '',
             email:'',
@@ -36,16 +36,21 @@ class Register extends Component {
             password: this.state.passOne
         }
         e.preventDefault();
-        firebase.auth().createUserWithEmailAndPassword(
-            registrationInfo.email,
-            registrationInfo.password
-        ).catch(error => {
-            if (error.message !== null) {
-                this.setState({errorMessage: error.message});
-            } else {
-                this.setState({errorMessage: null});
-            }
-        })
+        firebase
+            .auth()
+            .createUserWithEmailAndPassword(
+                registrationInfo.email,
+                registrationInfo.password
+            ).then (() => {
+                this.props.registerUser(registrationInfo.displayName);
+            })
+            .catch(error => {
+                if (error.message !== null) {
+                    this.setState({errorMessage: error.message});
+                } else {
+                    this.setState({errorMessage: null});
+                }
+            })
     }
 
     render(){
