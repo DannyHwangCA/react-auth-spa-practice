334779dc659f07bbd4a479dda15dc8a81c27d028
Danny Hwang <dannyhwangca@gmail.com>
2020-04-26 22:50:18 -0700

    added delete on click

--- testapp/src/MeetingsList.js
+++ testapp/src/MeetingsList.js
@@ -7,6 +7,17 @@ class MeetingsList extends Component {
         const myMeetings = meetings.map(item => {
             return(
                     <div className="list-group-item d-flex" key={item.meetingID}>
+
+                        <section className="btn-group align-self-center" role="group" aria-label="Meeting Options">
+                            <button className="btn btn-small btn-outline-secondary"
+                            title="Delete Meeting"
+                            onClick={e => this.deleteMeeting(e, item.meetingId)}
+                            >
+                                <GoTrashcan />
+                            </button>
+                        </section>
+
+
                         <section className="pl-3 text-left align-self-center">
                             {item.meetingName}
                         </section>
