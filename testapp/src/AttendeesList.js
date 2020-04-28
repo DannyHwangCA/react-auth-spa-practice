0b249aca9ca4ceec5f0b2b0f4619ff885dbbbcdc
Danny Hwang <dannyhwangca@gmail.com>
2020-04-27 23:34:58 -0700

    added delete attendee

--- testapp/src/AttendeesList.js
+++ testapp/src/AttendeesList.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { GoTrashcan } from 'react-icons/go';
 
 class AttendeesList extends React.Component {
     constructor(props) {
@@ -11,10 +12,20 @@ class AttendeesList extends React.Component {
         const myAttendees = attendees.map(item => {
             return(
                 <div className="col-8 col-sm-6 col-md-4 col-lg-3 mb-2 p-0 px-1" key={item.attendeeID}>
-                    <div className="card ">
-                        <div className="card-body px-3 py-2 d-flex align-items-center justify-content-center">
+                    <div className={
+                        'card-body px-3 py-2 d-flex align-items-center ' +
+                        (admin ? '': 'justify-content-center')
+                    }>
+                        {admin && (
+                            <div className="bin-group pr-2">
+                                <button className="btn btn-sm btn-outline-secondary"
+                                title="Delete Attendee"
+                                onClick={e => this.deleteAttendee(e, this.props.meetingID, item.attendeeID )}>
+                                    <GoTrashcan />
+                                </button>
+                            </div>
+                        )}
                             <div>{item.attendeeName}</div>
-                        </div>
                     </div>
                 </div>
             )
